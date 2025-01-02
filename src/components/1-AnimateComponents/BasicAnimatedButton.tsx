dab3ae43e25254b843bcec6e52218d41f38ca32c
Aman Sachan <codewithaman78@gmail.com>
2025-01-03 03:17:35 +0530

    ui(popup-modal): Added entry and exit animations

--- src/components/1-AnimateComponents/BasicAnimatedButton.tsx
+++ src/components/1-AnimateComponents/BasicAnimatedButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
+import { BsFillLightningFill } from "react-icons/bs";
 
 const BasicAnimatedButton = ({
   children,
@@ -21,21 +22,8 @@ const BasicAnimatedButton = ({
       >
         <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:-translate-x-1"></span>
         <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-1"></span>
-        <span className="relative z-20 flex items-center text-sm">
-          <svg
-            className="relative w-5 h-5 mr-2 text-white"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M13 10V3L4 14h7v7l9-11h-7z"
-            ></path>
-          </svg>
+        <span className="relative z-20 flex items-center text-sm gap-2">
+          <BsFillLightningFill className="size-4" />
           {children}
         </span>
       </motion.button>
