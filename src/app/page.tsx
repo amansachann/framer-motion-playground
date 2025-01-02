19fa1bf55219b2c48ade80024b6d09bf870a579b
Aman Sachan <codewithaman78@gmail.com>
2025-01-03 01:19:11 +0530

    Added: Basic Animated Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import BasicAnimatedButton from "@/components/BasicAnimatedButton";
 import BasicFadeInAnimation from "@/components/BasicFadeInAnimation";
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-screen flex items-center justify-center">
+      <div className="h-screen w-screen flex items-center justify-center flex-col gap-8">
         <BasicFadeInAnimation />
+        <BasicAnimatedButton/>
       </div>
     </>
   );
