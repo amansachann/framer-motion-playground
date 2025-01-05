5373806e3f783d747512ac665d3b4dfdb1ee327d
Aman Sachan <codewithaman78@gmail.com>
2025-01-06 04:24:03 +0530

    Add: Insta Typing

--- src/components/OtherAnimations/InstaTyping.tsx
+++ src/components/OtherAnimations/InstaTyping.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
+const parentVariants = {
+  initial: { y: 0 },
+  animate: { transition: { staggerChildren: 0.4 } },
+};
+
+const childVariants = {
+  initial: { y: 0 },
+  animate: {
+    y: [0, -10, 0],
+    transition: { repeat: Infinity, duration: 1.2, ease: "easeInOut" },
+  },
+};
 
 const InstaTyping = () => {
   return (
-    <div>InstaTyping</div>
-  )
-}
+    <>
+      <motion.section
+        className="flex gap-4 bg-gray-800 p-8 rounded-full"
+        variants={parentVariants}
+        initial="initial"
+        animate="animate"
+      >
+        {[...Array(3)].map((_, index) => (
+          <motion.div
+            key={index}
+            className="h-8 w-8 rounded-full bg-gradient-to-r from-orange-400 via-pink-500 to-purple-500"
+            variants={childVariants}
+          />
+        ))}
+      </motion.section>
+    </>
+  );
+};
 
-export default InstaTyping
+export default InstaTyping;
